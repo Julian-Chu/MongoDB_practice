1d595a8861e8de60f3ae5d64d9bd54dcc1d6a880
JulianChu <yulang.chu@gmail.com>
2018-01-18 01:11:58 +0100

    ListDBs, CreateACollection

--- MongoDB_practice/MongoDBRepository.cs
+++ MongoDB_practice/MongoDBRepository.cs
@@ -6,36 +6,33 @@ using System.Text;
 
 namespace MongoDB_practice
 {
-  internal class MongoDBRepository<T> where T : Equipment
-  {
-    private MongoClient client;
-    private IMongoDatabase database;
-    private IMongoCollection<T> collection;
-
-    public MongoDBRepository(string connectionString)
+    internal class MongoDBRepository<T> where T : Equipment
     {
-      client = new MongoClient(connectionString);
-      database = client.GetDatabase("local");
-      collection = database.GetCollection<T>(nameof(T));
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<T> collection;
 
+        public MongoDBRepository(string connectionString) {
+            client = new MongoClient(connectionString);
+            database = client.GetDatabase("local");
+            collection = database.GetCollection<T>(nameof(T));
+
+        }
+        public void Add(T item) {
+
+            collection.InsertOne(item);
+        }
+
+        public void Update(T item) {
+            var builder = Builders<T>.Filter;
+            var filter = builder.Eq(eq => eq.id, item.id);
+            var update = Builders<T>.Update.Set(s => s.name, item.name);
+
+            //collection.UpdateOne(filter,update);
+            collection.FindOneAndUpdate<T>(filter, update);
+
+        }
     }
-    public void Add(T item)
-    {
-
-      collection.InsertOne(item);
-    }
-
-    public void Update(T item)
-    {
-      var builder = Builders<T>.Filter;
-      var filter = builder.Eq(eq => eq.id, item.id);
-      var update = Builders<T>.Update.Set(s => s.name, item.name);
-
-      //collection.UpdateOne(filter,update);
-      collection.FindOneAndUpdate<T>(filter, update);
-
-    }
-  }
 
     internal class MongoDBRepository
     {
@@ -58,7 +55,7 @@ namespace MongoDB_practice
         }
 
         public long CountDocuments() {
-            return collection.Count<Equipment>(eq=>eq.id > 1);
+            return collection.Count<Equipment>(eq => eq.id > 1);
         }
 
         public Equipment GetById(int id) {
@@ -69,5 +66,30 @@ namespace MongoDB_practice
             // give empty filter, then return all documents
             return collection.Find(Builders<Equipment>.Filter.Empty).ToEnumerable();
         }
-  }
+
+        public void ListDBs() {
+            using(var dbs = client.ListDatabases()) {
+                foreach(var db in dbs.ToEnumerable()) {
+                    Console.WriteLine(db.ToString());
+                }
+            }
+        }
+
+        public void CreateACollection(string name) {
+            var filter = new BsonDocument("name", name);
+            var isCollectionExisting = database.ListCollections(new ListCollectionsOptions { Filter = filter }).AnyAsync().Result;
+
+            if(!isCollectionExisting) {
+                var options = new CreateCollectionOptions { Capped = true, MaxSize = 1024 * 1024 };
+                database.CreateCollection(name, options);
+            } else {
+                Console.WriteLine("Collection exists already ");
+            }
+
+            
+
+        }
+
+
+    }
 }
--- MongoDB_practice/Program.cs
+++ MongoDB_practice/Program.cs
@@ -7,6 +7,9 @@ namespace MongoDB_practice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var repo = new MongoDBRepository("mongodb://localhost:27017");
+            repo.ListDBs();
+            repo.CreateACollection("Equipment");
         }
     }
 }
